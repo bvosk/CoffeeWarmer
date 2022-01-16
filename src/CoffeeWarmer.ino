a637b551a97db71cfc0b4619e9812c4e85842309
Brian Voskerijian <brian.voskerijian@7factor.io>
2022-01-15 22:04:24 -0500

    PID loop working

--- src/CoffeeWarmer.ino
+++ src/CoffeeWarmer.ino
@@ -1,22 +1,41 @@
 #include "DS18.h"
+#include "pid.h"
 
 SerialLogHandler logHandler;
 
 DS18 temperatureSensor(D0);
 double temperature = 0;
 
-int ledRelay = D7;
+int heatCartridgeRelay = D7;
+
+double desiredTemperature;
+const int kp = 1, ki = 1, kd = 1;
+int WindowSize = 5000;
+double pidOutput = WindowSize / 2;
+PID temperaturePidController(&temperature, &pidOutput, &desiredTemperature,kp,ki,kd, PID::DIRECT);
+unsigned long windowStartTime;
 
 void setup() {
   Particle.variable("temperature", temperature);
-  pinMode(ledRelay, OUTPUT); 
+  pinMode(heatCartridgeRelay, OUTPUT); 
+
+  windowStartTime = millis();
+  //initialize the variables we're linked to
+  desiredTemperature = 98;
+
+  //tell the PID to range between 0 and the full window size
+  temperaturePidController.SetOutputLimits(0, 1);
+
+  //turn the PID on
+  temperaturePidController.SetMode(PID::AUTOMATIC);
 }
 
 void loop() {
   readTemperature();
-  toggleRelay();
-  
-  delay(2000);
+  controlRelayDumb();
+  printDetails();
+
+  delay(1000);
 }
 
 void readTemperature() {
@@ -25,13 +44,32 @@ void readTemperature() {
   }
 }
 
-void toggleRelay() {
-  const PinState relayState = (PinState) digitalRead(ledRelay);
-  if (relayState == LOW) {
-    Log.info("Turning relay on.");
-    digitalWrite(ledRelay, HIGH);
-  } else {
-    Log.info("Turning relay off.");
-    digitalWrite(ledRelay, LOW);
+void controlRelayDumb() {
+  temperaturePidController.Compute();
+  digitalWrite(heatCartridgeRelay, (PinState) pidOutput);
+}
+
+void printDetails() {
+  Log.info(String::format("Current Temperature: %f", temperature));
+  Log.info(String::format("PID Output: %f", pidOutput));
+  Log.info(String::format("Relay Output: %f", pidOutput));
+}
+
+void controlRelay() {
+  temperaturePidController.Compute();
+  /************************************************
+   * turn the output pin on/off based on pid output
+   ************************************************/
+  if ((millis() - windowStartTime) > WindowSize) {
+    //time to shift the Relay Window
+    windowStartTime += WindowSize;
+    Log.info(String::format("New window started at: %ims", windowStartTime));
+    Log.info(String::format("Relay duty cycle: %f%", (pidOutput / WindowSize) * 100));
+  }
+  if (pidOutput < (millis() - windowStartTime)) {
+    digitalWrite(heatCartridgeRelay, LOW);
+  }
+  else {
+    digitalWrite(heatCartridgeRelay, HIGH);
   }
 }
